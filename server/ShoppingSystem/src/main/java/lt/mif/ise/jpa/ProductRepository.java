e9b90136d916506b190ea5b61cd6d8132fe1340f
Arvydas Silanskas <arvydas@ArvydoDebian>
2018-04-29 22:58:13 +0300

    GUI search + paging

--- server/ShoppingSystem/src/main/java/lt/mif/ise/jpa/ProductRepository.java
+++ server/ShoppingSystem/src/main/java/lt/mif/ise/jpa/ProductRepository.java
@@ -2,6 +2,8 @@ package lt.mif.ise.jpa;
 
 import lt.mif.ise.domain.Product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,4 +11,5 @@ import java.util.List;
 
 public interface ProductRepository extends CrudRepository<Product, String>, JpaSpecificationExecutor<Product>{
     List<Product> findByName(String name);
+    Page<Product> findAll(Pageable page);
 }
--- server/ShoppingSystem/src/main/java/lt/mif/ise/service/ProductService.java
+++ server/ShoppingSystem/src/main/java/lt/mif/ise/service/ProductService.java
@@ -5,10 +5,13 @@ import lt.mif.ise.domain.search.ProductCriteria;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ProductService {
     Product getById(String id);
     
-    Iterable<Product> findProducts(ProductCriteria search);
+    Page<Product> findProducts(ProductCriteria search, Pageable page);
 
     Product create(Product product);
 

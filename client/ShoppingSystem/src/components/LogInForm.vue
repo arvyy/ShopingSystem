d5f92c96f77b61cdce783d67e7437b9c6b85a865
DESKTOP\Sarunas <visarunas@gmail.com>
2018-04-29 16:08:06 +0300

    log in form submit

--- client/ShoppingSystem/src/components/LogInForm.vue
+++ client/ShoppingSystem/src/components/LogInForm.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="login_form">
-    <form action="/login" method="post">
+    <form action="http://localhost:8080/login" method="POST">
       <input type="text" name="username" placeholder="User Name">
       <input type="password" name="password" placeholder="Password">
-      <input type="submit" value="Submit">
+      <input type="submit" value="Login">
     </form>
   </div>
 </template>

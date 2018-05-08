750f49a580b27eb5524ea475ce2b47474c5fdfb8
Justas <justasrasl@yahoo.com>
2018-05-08 12:52:59 +0300

    Fixed auto login after successful registration.

--- server/ShoppingSystem/src/main/java/lt/mif/ise/rest/controller/UserController.java
+++ server/ShoppingSystem/src/main/java/lt/mif/ise/rest/controller/UserController.java
@@ -2,19 +2,20 @@ package lt.mif.ise.rest.controller;
 
 import lt.mif.ise.domain.User;
 import lt.mif.ise.security.UserValidator;
-import lt.mif.ise.service.SecurityService;
 import lt.mif.ise.service.UserService;
-import org.hibernate.id.GUIDGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import java.security.Principal;
@@ -24,8 +25,8 @@ import java.util.UUID;
 @RestController
 public class UserController {
     
-//	@Autowired
-//    private SecurityService securityService;
+	@Autowired
+    private AuthenticationManager authenticationManager;
 
     @Autowired
     private UserService userService;
@@ -33,22 +34,37 @@ public class UserController {
     @Autowired
     private UserValidator userValidator;
 
-//    @RequestMapping(value="sign-up", method = RequestMethod.POST)
-//    public ResponseEntity signUp(@RequestBody @Valid User user, BindingResult bindingResult){
-//        userValidator.validate(user, bindingResult);
-//
-//        if (bindingResult.hasErrors()) {
-//            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-//        }
-//
-//        user.setId(UUID.randomUUID().toString());
-//        user.setEnabled(true);
-//        userService.save(user);
-//
-//        securityService.autologin(user.getEmail(), user.getPassword());
-//
-//        return new ResponseEntity(HttpStatus.CREATED);
-//    }
+    @RequestMapping(value="sign-up", method = RequestMethod.POST)
+    public ResponseEntity signUp(@RequestBody @Valid User user, BindingResult bindingResult, HttpServletRequest request){
+        userValidator.validate(user, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        String email = user.getEmail();
+        String password = user.getPassword();
+
+        user.setId(UUID.randomUUID().toString());
+        user.setEnabled(true);
+        userService.save(user);
+
+        autoLogin(email, password, request);
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    public boolean autoLogin( String username, String password, HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+
+        Authentication authentication = authenticationManager.authenticate(token);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication );
+        request.getSession().setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
+
+        return true;
+    }
+
     
     @RequestMapping("me")
     public String me(Principal p) {

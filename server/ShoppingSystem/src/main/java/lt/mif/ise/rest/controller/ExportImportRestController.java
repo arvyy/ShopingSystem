1aab7b91a6b8c04088c38b96e5e013578916190b
sarunassp <sarunassp@gmail.com>
2018-05-20 22:40:37 +0300

    dont create file while importting/exporting
    import optimization

--- server/ShoppingSystem/src/main/java/lt/mif/ise/rest/controller/ExportImportRestController.java
+++ server/ShoppingSystem/src/main/java/lt/mif/ise/rest/controller/ExportImportRestController.java
@@ -1,71 +1,43 @@
 package lt.mif.ise.rest.controller;
 
 import lt.mif.ise.bean.ExportImport;
-import lt.mif.ise.jpa.ProductRepository;
+import lt.mif.ise.domain.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
 
 @RestController
-@RequestMapping("api/excel/")
+@RequestMapping("api/excel")
 public class ExportImportRestController {
-    @Autowired
-    private ProductRepository productRepository;
-
     @Autowired
     private ExportImport exportImport;
 
-    private void saveFile(InputStream inputStream, String path){
-        try {
-            OutputStream outputStream = new FileOutputStream(new File(path));
-            int read = 0;
-            byte[] bytes = new byte[1024];
-            while ((read = inputStream.read(bytes)) != -1){
-                outputStream.write(bytes, 0, read);
-            }
-
-            outputStream.flush();
-            outputStream.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void importExcel(String path){
-        try {
-            exportImport.importProducts(path);
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
     @RequestMapping(value = "upload", method = RequestMethod.POST)
-    private String upload(@RequestParam(value = "file")MultipartFile file, HttpServletRequest request){
+    public Iterable<Product> upload(@RequestParam(value = "file")MultipartFile file){
         try {
-            String fileName= file.getOriginalFilename();
-            String path = request.getServletContext().getRealPath("") + fileName;
-            System.out.println(path);
-            saveFile(file.getInputStream(), path);
-            importExcel(path);
-            return fileName;
+            return exportImport.importProducts(file.getInputStream());
         } catch(Exception e) {
-            return null;
+            throw new RuntimeException(e.getMessage());
         }
     }
 
     @RequestMapping(value = "export", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     @ResponseBody
-    private FileSystemResource export() {
-        String path = exportImport.exportProducts();
-        return new FileSystemResource(path);
+    public ResponseEntity<ByteArrayResource> export(HttpServletResponse response) {
+        try {
+            ByteArrayOutputStream stream = exportImport.exportProducts();
+            ByteArrayResource resource = new ByteArrayResource(stream.toByteArray());
+            response.setHeader("Content-Disposition", "attachment; filename=export.xlsx");
+            return new ResponseEntity<>(resource, HttpStatus.OK);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }

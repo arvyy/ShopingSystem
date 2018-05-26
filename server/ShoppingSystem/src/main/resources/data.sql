bb6fab2099d6d85fe520a9125eab958e3ac259ed
Justas <justasrasl@yahoo.com>
2018-05-26 22:01:58 +0300

    Added admin user reading from application properties file.

--- server/ShoppingSystem/src/main/resources/data.sql
+++ server/ShoppingSystem/src/main/resources/data.sql
@@ -1,7 +1,3 @@
 -- Insert default user roles:
 MERGE INTO ROLE VALUES (1, 'ROLE_USER')
 MERGE INTO ROLE VALUES (2, 'ROLE_ADMIN')
-
--- Create admin user (guid, email, enabled, password) and add ROLE_ADMIN to it
-MERGE INTO USER VALUES('03dcd982-7445-44d2-a27e-d5addac9754e', 'admin@gmail.com', TRUE,'$2a$04$DjA.eKOkokIEKD0Gfqy2x.ZQX44x8uFG1QYQQ4ztKxCgZYHuv4AFy')
-MERGE INTO USER_ROLE VALUES('03dcd982-7445-44d2-a27e-d5addac9754e', '2')

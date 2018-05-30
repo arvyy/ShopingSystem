7a4b2e3f17573fcf5d97fd33f9d224c687de7278
sarunassp <sarunassp@gmail.com>
2018-05-30 22:33:05 +0300

    fixing authorization for shoppnig cart

--- server/ShoppingSystem/src/main/java/lt/mif/ise/rest/controller/ShoppingCartRestController.java
+++ server/ShoppingSystem/src/main/java/lt/mif/ise/rest/controller/ShoppingCartRestController.java
@@ -19,7 +19,6 @@ public class ShoppingCartRestController {
     @Autowired
     private ShoppingCartService shoppingCart;
 
-    @PreAuthorize("hasAnyRole('ADMIN') or hasAnyRole('USER')")
     @RequestMapping(method = RequestMethod.POST)
     public Iterable<Object[]> addToCart(@RequestBody Iterable<ProductForCart> products){
         for (ProductForCart product: products) {
@@ -28,13 +27,11 @@ public class ShoppingCartRestController {
         return getCart();
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN') or hasAnyRole('USER')")
     @RequestMapping(method = RequestMethod.DELETE, value = "{productId}")
     public void deleteFromCart(@PathVariable(value = "productId") String productId){
         shoppingCart.removeFromCart(productId);
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN') or hasAnyRole('USER')")
     @RequestMapping(method = RequestMethod.DELETE, value = "{productId}/{amount}")
     public ProductForCart deleteFromCart
     (
@@ -48,13 +45,11 @@ public class ShoppingCartRestController {
         return deletedProduct;
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN') or hasAnyRole('USER')")
     @RequestMapping(method = RequestMethod.DELETE)
     public void clearCart() {
     	shoppingCart.clearCart();
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN') or hasAnyRole('USER')")
     @RequestMapping(method = RequestMethod.GET)
     public Iterable<Object[]> getCart(){
         Iterable<Pair<Product, Integer>> cart = shoppingCart.getCart();
@@ -65,24 +60,5 @@ public class ShoppingCartRestController {
             });
         });
         return forDeserializing;
-    }
-
-    //test method
-    //TODO delete before PROD
-/*
- * [[product, count], [product2, count2], ...]
- */
-    @RequestMapping(method = RequestMethod.GET, value = "/testingData")
-    public Iterable<Object[]> getCartTestingData(){
-        //return shoppingCart.getCart();
-    	return IntStream.range(0, 5).mapToObj(i -> {
-    		Product p = new Product();
-    		p.setProductId("prod-" + i);
-    		p.setName("Randomname" + p.hashCode());
-    		p.setPrice(new BigDecimal(1.99));
-    		return new Object[]{
-    			p, i
-    		};
-    	}).collect(Collectors.toList());
     }
 }

215ef43d46880821b21a8a12554307046fb3aff1
Justas <justasrasl@yahoo.com>
2018-05-30 18:12:01 +0300

    Improved logging.

--- server/ShoppingSystem/src/main/java/lt/mif/ise/logging/MethodLogger.java
+++ server/ShoppingSystem/src/main/java/lt/mif/ise/logging/MethodLogger.java
@@ -1,7 +1,11 @@
 package lt.mif.ise.logging;
 
+import org.apache.catalina.security.SecurityUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -13,15 +17,15 @@ public class MethodLogger extends HandlerInterceptorAdapter{
     private static final Logger logger = LoggerFactory.getLogger(MethodLogger.class);
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
         long startTime = System.currentTimeMillis();
 
         logger.info("\n\n-------- Logger.preHandle --- " +
                     "\nRequest URL: " + request.getRequestURL() +
                     "\nStart Time: " + System.currentTimeMillis() +
                 ((null != request.getUserPrincipal()) ?
-                    "\nPrincipal: " + request.getUserPrincipal() : "") +
+                    "\nPrincipal: " + request.getUserPrincipal().getName() +
+                    "\nAuthorities: " + SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString() : "") +
                     "\nMethod: " + request.getMethod());
 
         request.setAttribute("startTime", startTime);
@@ -31,13 +35,13 @@ public class MethodLogger extends HandlerInterceptorAdapter{
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, //
-                           Object handler, ModelAndView modelAndView) throws Exception {
+                           Object handler, ModelAndView modelAndView){
 
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, //
-                                Object handler, Exception ex) throws Exception {
+                                Object handler, Exception ex) {
         logger.info("\n\n-------- Logger.postHandle --- " +
                     "\nRequest URL: " + request.getRequestURL() +
                     "\nSTATUS: " + response.getStatus() +
